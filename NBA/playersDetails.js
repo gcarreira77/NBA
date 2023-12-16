5da6894a748be42f7fb6edabd41021ed6b824007
gcarreira77 <152729579+gcarreira77@users.noreply.github.com>
2023-12-16 23:35:49 +0000

    a

--- NBA/playersDetails.js
+++ NBA/playersDetails.js
@@ -24,6 +24,20 @@ var vm = function () {
     self.Seasons = ko.observableArray([])
     self.Teams = ko.observableArray([]);
     
+    self.stats = function (id) {
+        var statUri = 'http://192.168.160.58/NBA/API/Statistics/NumPlayersBySeason' + id;
+        self.SeasonType = ko.observableArray([]);
+        self.Season = ko.observableArray([])
+        self.Players = ko.observable('');
+        ajaxHelper(statUri, 'GET').done(function (statData) {
+            console.log(statData);
+            hideLoading();
+            self.Season(data.Season);
+            self.SeasonType(data.SeasonType);
+            self.Players(data.Players);
+        });
+    };
+
 
     //--- Page Events
     self.activate = function (id) {
@@ -47,6 +61,7 @@ var vm = function () {
             self.Biography(data.Biography);
             self.Seasons(data.Seasons);
             self.Teams(data.Teams);
+            self.stats(id);
 
         });
     };
@@ -111,93 +126,9 @@ var vm = function () {
 };
 
 
-// Team ViewModel
-var viewm = function () {
-    console.log('ViewModel initiated...');
-    //---Variáveis locais
-    var self = this;
-    self.baseUri = ko.observable('http://192.168.160.58/NBA/API/Statistics/NumPlayersBySeason');
-    self.displayName = 'NBA Players Details';
-    self.error = ko.observable('');
-    self.passingMessage = ko.observable('');
-    //--- Data Record
-    self.SeasonType = ko.observableArray([]);
-    self.Season = ko.observableArray([])
-    self.Players = ko.observable('');
-    
-
-    //--- Page Events
-    self.activate = function (id) {
-        console.log('CALL: getStats...');
-        var composedUri = self.baseUri() + id;
-        ajaxHelper(composedUri, 'GET').done(function (data) {
-            console.log(data);
-            hideLoading();
-            self.Season(data.Season);
-            self.SeasonType(data.SeasonType);
-            self.Players(data.Players);
-
-        });
-    };
-    
-
-    //--- Internal functions
-    function ajaxHelper(uri, method, data) {
-        self.error(''); // Clear error message
-        return $.ajax({
-            type: method,
-            url: uri,
-            dataType: 'json',
-            contentType: 'application/json',
-            data: data ? JSON.stringify(data) : null,
-            error: function (jqXHR, textStatus, errorThrown) {
-                console.log("AJAX Call[" + uri + "] Fail...");
-                hideLoading();
-                self.error(errorThrown);
-            }
-        });
-    }
-
-    function showLoading() {
-        $('#myModal').modal('show', {
-            backdrop: 'static',
-            keyboard: false
-        });
-    }
-    function hideLoading() {
-        $('#myModal').on('shown.bs.modal', function (e) {
-            $("#myModal").modal('hide');
-        })
-    }
-
-    function getUrlParameter(sParam) {
-        var sPageURL = window.location.search.substring(1),
-            sURLVariables = sPageURL.split('&'),
-            sParameterName,
-            i;
-
-        for (i = 0; i < sURLVariables.length; i++) {
-            sParameterName = sURLVariables[i].split('=');
-
-            if (sParameterName[0] === sParam) {
-                return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
-            }
-        }
-    };
-
-
-}
-
 $(document).ready(function () {
     console.log("document.ready!");
-
-    // Create instances of both view models
-    var playerVM = new vm();
-    var statVM = new viewm();
-
-    // Apply bindings for both view models
-    ko.applyBindings(vm, document.getElementById('players'));
-    ko.applyBindings(viewm, document.getElementById('stats'));
+    ko.applyBindings(new vm());
 });
 
 $(document).ajaxComplete(function (event, xhr, options) {

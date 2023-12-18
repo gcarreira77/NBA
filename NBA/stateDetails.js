bdf28399c6ac92805b32d4fd69408424afdb159b
gcarreira77 <152729579+gcarreira77@users.noreply.github.com>
2023-12-18 10:51:09 +0000

    a

--- NBA/stateDetails.js
+++ NBA/stateDetails.js
@@ -1,25 +1,36 @@
-﻿// ViewModel KnockOut
+// ViewModel KnockOut
 var vm = function () {
     console.log('ViewModel initiated...');
     //---Variáveis locais
     var self = this;
-    self.baseUri = ko.observable('http://192.168.160.58/NBA/API/Seasons/');
-    self.displayName = 'NBA Season Details';
+    self.baseUri = ko.observable('http://192.168.160.58/NBA/API/Arenas/');
+    self.displayName = 'NBA Arena Details';
     self.error = ko.observable('');
     self.passingMessage = ko.observable('');
     //--- Data Record
     self.Id = ko.observable('');
-    self.Season = ko.observable('');
+    self.Name = ko.observable('');
+    self.Flag = ko.observable('');
+    self.Teams = ko.observableArray([]);
+    self.Arenas = ko.observableArray([]);
+
     //--- Page Events
     self.activate = function (id) {
-        console.log('CALL: getSeasons...');
+        console.log('CALL: getStates...');
         var composedUri = self.baseUri() + id;
         ajaxHelper(composedUri, 'GET').done(function (data) {
             console.log(data);
             hideLoading();
             self.Id(data.Id);
-            self.Season(data.Season);
-
+            self.Name(data.Name);
+            self.Flag(data.Flag);
+            self.StateName(data.StateName);
+            self.Teams(data.TeamId);
+            self.Arenas(data.Arenas);
+        }).fail(function (jqXHR, textStatus, errorThrown) {
+            console.log("AJAX Call[" + composedUri + "] Fail...");
+            hideLoading();
+            self.error("Error fetching data: " + errorThrown);
         });
     };
 
@@ -31,12 +42,7 @@ var vm = function () {
             url: uri,
             dataType: 'json',
             contentType: 'application/json',
-            data: data ? JSON.stringify(data) : null,
-            error: function (jqXHR, textStatus, errorThrown) {
-                console.log("AJAX Call[" + uri + "] Fail...");
-                hideLoading();
-                self.error(errorThrown);
-            }
+            data: data ? JSON.stringify(data) : null
         });
     }
 
@@ -46,10 +52,9 @@ var vm = function () {
             keyboard: false
         });
     }
+
     function hideLoading() {
-        $('#myModal').on('shown.bs.modal', function (e) {
-            $("#myModal").modal('hide');
-        })
+        $("#myModal").modal('hide');
     }
 
     function getUrlParameter(sParam) {
@@ -65,7 +70,7 @@ var vm = function () {
                 return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
             }
         }
-    };
+    }
 
     //--- start ....
     showLoading();
@@ -86,4 +91,4 @@ $(document).ready(function () {
 
 $(document).ajaxComplete(function (event, xhr, options) {
     $("#myModal").modal('hide');
-})
+});

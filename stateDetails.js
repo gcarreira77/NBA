98a659b2e414e23bb45fb241e05e300f189cabc8
FilipeNogue1ra <filipe.nogueira@ua.pt>
2023-12-18 08:09:14 +0000

    asd

---
+++ stateDetails.js
@@ -0,0 +1,94 @@
+// ViewModel KnockOut
+var vm = function () {
+    console.log('ViewModel initiated...');
+    //---Variáveis locais
+    var self = this;
+    self.baseUri = ko.observable('http://192.168.160.58/NBA/API/Arenas/');
+    self.displayName = 'NBA Arena Details';
+    self.error = ko.observable('');
+    self.passingMessage = ko.observable('');
+    //--- Data Record
+    self.Id = ko.observable('');
+    self.Name = ko.observable('');
+    self.Flag = ko.observable('');
+    self.Teams = ko.observableArray([]);
+    self.Arenas = ko.observableArray([]);
+
+    //--- Page Events
+    self.activate = function (id) {
+        console.log('CALL: getStates...');
+        var composedUri = self.baseUri() + id;
+        ajaxHelper(composedUri, 'GET').done(function (data) {
+            console.log(data);
+            hideLoading();
+            self.Id(data.Id);
+            self.Name(data.Name);
+            self.Flag(data.Flag);
+            self.StateName(data.StateName);
+            self.Teams(data.TeamId);
+            self.Arenas(data.Arenas);
+        }).fail(function (jqXHR, textStatus, errorThrown) {
+            console.log("AJAX Call[" + composedUri + "] Fail...");
+            hideLoading();
+            self.error("Error fetching data: " + errorThrown);
+        });
+    };
+
+    //--- Internal functions
+    function ajaxHelper(uri, method, data) {
+        self.error(''); // Clear error message
+        return $.ajax({
+            type: method,
+            url: uri,
+            dataType: 'json',
+            contentType: 'application/json',
+            data: data ? JSON.stringify(data) : null
+        });
+    }
+
+    function showLoading() {
+        $('#myModal').modal('show', {
+            backdrop: 'static',
+            keyboard: false
+        });
+    }
+
+    function hideLoading() {
+        $("#myModal").modal('hide');
+    }
+
+    function getUrlParameter(sParam) {
+        var sPageURL = window.location.search.substring(1),
+            sURLVariables = sPageURL.split('&'),
+            sParameterName,
+            i;
+
+        for (i = 0; i < sURLVariables.length; i++) {
+            sParameterName = sURLVariables[i].split('=');
+
+            if (sParameterName[0] === sParam) {
+                return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+            }
+        }
+    }
+
+    //--- start ....
+    showLoading();
+    var pg = getUrlParameter('id');
+    console.log(pg);
+    if (pg == undefined)
+        self.activate(1);
+    else {
+        self.activate(pg);
+    }
+    console.log("VM initialized!");
+};
+
+$(document).ready(function () {
+    console.log("document.ready!");
+    ko.applyBindings(new vm());
+});
+
+$(document).ajaxComplete(function (event, xhr, options) {
+    $("#myModal").modal('hide');
+});
